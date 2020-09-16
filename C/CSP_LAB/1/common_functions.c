22f2ed0ca90e348121350bb9ccfeb2e2625c5a20
Shantanu2508 <shantanu0898@gmail.com>
2020-09-16 19:57:15 +0530

    Update

--- C/CSP_LAB/1/common_functions.c
+++ C/CSP_LAB/1/common_functions.c
@@ -28,48 +28,39 @@ float *corr(float x[],float y[],int x_len,int y_len)
 {
 	int n = x_len+y_len-1;
 	float sum=0;
-	float *yy[n],*Rxy,*xx;
+	float *yy[n],*Rxy;
 	Rxy=(float*)calloc(n,sizeof(float));
-	xx=(float*)calloc(n,sizeof(float));
-	
 
 	//creating nxn square matrix with all zeros
-	for(int i=0,j=x_len-1;i<n,j>=0;i++,j--) xx[i] = x[j];
+
 	for(int i=0;i<n;i++)
 		yy[i]=(float*)calloc(n,sizeof(float));
 	
 	//Initializing first column of yy as y and then shifting it : print yy to see
 	for(int i=0;i<y_len;i++) yy[i][0] = y[i];
-	
+	for(int i=0,j=y_len-1;i<n,j>=0;i++,j--) *yy[i] = y[j];
 	
 	for(int j=1;j<n;j++)
 	{
 		for(int i=1;i<n;i++)
 			yy[i][j] = yy[i-1][j-1];
 	}
-	
-	printf("\n\n");
+
 	//.........................................................................
 
 	for(int i=0;i<n;i++)
 	{
 		for(int j=0;j<n;j++)
 		{
-			sum= sum+ yy[i][j]*xx[j];
+			sum= sum+ yy[i][j]*x[j];
 		}
 
-		Rxy[n-1-i] = sum;
+		Rxy[i] = sum;
 		sum=0;
 	}
 	//...........................................................................
 	return Rxy;
 }
-
-
-
-
-
-
 
 float *downsampling(float x[], int x_len, int sampling_factor)
 {
